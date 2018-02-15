65f00a149dbdce60f4e858531354f11f9ea75eeb
Abby Malson <abby@augustmalson.com>
2018-02-14 22:06:03 -0700

    work in pgoress

---
+++ elixir/secret-handshake/secret_handshake.exs
@@ -0,0 +1,23 @@
+defmodule SecretHandshake do
+  @doc """
+  Determine the actions of a secret handshake based on the binary
+  representation of the given `code`.
+
+  If the following bits are set, include the corresponding action in your list
+  of commands, in order from lowest to highest.
+
+  1 = wink
+  10 = double blink
+  100 = close your eyes
+  1000 = jump
+
+  10000 = Reverse the order of the operations in the secret handshake
+  """
+  @spec commands(code :: integer) :: list(String.t())
+  def commands(code) do
+    # convert code to binary string
+    binary_string = Integer.to_charlist(code, 2)
+    # Check for Wink
+  end
+end
+
