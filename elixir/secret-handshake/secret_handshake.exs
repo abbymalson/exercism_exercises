9612c30ccd6c0fe70b01615a0844779ec08a2146
Abby Malson <abby@augustmalson.com>
2018-02-14 22:29:38 -0700

    secret handshake start

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -16,8 +16,25 @@ defmodule SecretHandshake do
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
     # convert code to binary string
-    binary_string = Integer.to_charlist(code, 2)
-    # Check for Wink
+    binary_char_list = Integer.to_charlist(code, 2)
+
+    # store the results of the checks in a string/array/list so we can revers the action if there is a NOT
+
+    # Find the length of the string?
+    # should be able to
+    count(binary_char_list)
+    # ...
+    # Enum.map(Integer.to_charlist(code, 2))
+    Enum.map(binary_char_list)
+    # Enum.slice(binary_char_list, position)
+    # if 5 characters, and that value is a '1' then flip the results ...
+
+    # if at position 4th position jump (and value is '1') - Jump
+
+    # if at 3rd position - Close your eyes
+
+    # if at 2nd position - double blink
+    # if at 1st position - Check for Wink
   end
 end
 
